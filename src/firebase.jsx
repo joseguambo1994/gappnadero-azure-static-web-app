1819fecdd3a4b792024be34599d21082da66b09b
joseguambo1994 <jlguambo@gmail.com>
2025-03-13 00:37:01 -0500

    chore: Add testing values.

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log('test', firebaseConfig);
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
