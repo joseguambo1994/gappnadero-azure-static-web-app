12abe2ba70d61e7089276d3b38af1085bda3db76
joseguambo1994 <jlguambo@gmail.com>
2025-03-13 00:46:45 -0500

    fix: Prevet VITE variables override azure en variables.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   assetsInclude: ["**/*.lottie"],
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  }
 })
